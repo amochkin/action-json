182b847a5cd2d860e7ebf85c8d75236f42155a92
amochkin <amochkin@gmail.com>
2023-02-17 14:29:41 +0300

    fix: no output

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { run } from './action';
 
 if (require.main === module) {
 	// eslint-disable-next-line no-console
+	console.log('Running action (via node)...');
+} else {
+	// eslint-disable-next-line no-console
 	console.log('Running action...');
-	run();
 }
+run();
 
 export default run;
